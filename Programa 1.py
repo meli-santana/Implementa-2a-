3898cabb4e77e6c0d338a538134b77265e436279
Josue2012 <josueguadalupealvaradomendoza@gmail.com>
2025-03-20 19:23:37 -0600

    Add files via upload

---
+++ Programa 1.py
@@ -0,0 +1,9 @@
+z = 7
+if z > 8:
+ print("¡No voy a imprimir!") #esta sentencia no se ejecuta 
+elif z > 5:
+ print("¡Yo lo are!") #esta sencencia se ejecuta 
+elif z >6:
+ print("¡Tampoco voy a imprimir!") #esta sentencia no se ejecuta 
+else:
+ print("¡Yo tampoco!") #esta no se ejecuta
